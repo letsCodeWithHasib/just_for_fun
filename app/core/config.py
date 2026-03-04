3e83a4ffc9b87b3b677a6c867c029131885019ee
HashCoder <hashcoderslab@gmail.com>
2026-03-04 10:55:07 +0530

    fixed

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
-    database_url: str = "postgresql://postgres:12345@localhost:5432/dummy"
+    database_url: str = "postgresql://postgres.diglyrhbzpdcnlcimdiz:/v/Ht8gdAb6j_ek@aws-1-ap-south-1.pooler.supabase.com:5432/postgres"
     secret_key: str = "change_this_to_a_long_random_secret"
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 60
